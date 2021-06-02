d2e8c3f9b6063a432471131e7be8327639c8daed
Ajay Kumar Verma <66169115+AjayKumarVerma4@users.noreply.github.com>
2021-06-02 13:01:19 +0530

    Update SpringBootEmployeeApplication.java

--- src/main/java/com/hcl/SpringBootEmployeeApplication.java
+++ src/main/java/com/hcl/SpringBootEmployeeApplication.java
@@ -2,9 +2,17 @@ package com.hcl;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
-public class SpringBootEmployeeApplication {
+public class SpringBootEmployeeApplication extends SpringBootServletInitializer {
+	
+        @Override
+        protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+                  return application.sources(SpringBootEmployeeApplication.class);
+        }
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootEmployeeApplication.class, args);
